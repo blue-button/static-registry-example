ce0092e12dfd60f283be6699a2e3983c7bc2c507
Josh Mandel <joshua.mandel@childrens.harvard.edu>
2013-05-22 12:40:17 -0700

    JWKS uses base64-encoded n,e params for RSA keys

--- src/generate.js
+++ src/generate.js
@@ -4,8 +4,8 @@ var fs = require("fs");
 var path = require("path");
 var registry = require("../.well-known/bb/registry.json");
 var apps = require("../.well-known/bb/apps.json");
-
 var kid = "main-sample-key-"+new Date().toISOString().substr(0,10);
+var BigInteger = require("./node_modules/jwcrypto/libs/all").BigInteger;
 
 // jscrypto doesn't automatically register its algs -- 
 // but requiring an alg has the side effect of registering it...
@@ -13,10 +13,8 @@ require("./node_modules/jwcrypto/lib/algs/rs");
 
 jwcrypto.generateKeypair({
   algorithm: 'RS',
-  keysize: 256
+  keysize: 256 // --> 2048 bit RSA key!
 }, function(err, keypair) {
-
-  var pubkey = keypair.publicKey.toSimpleObject();
 
   fs.writeFileSync(
     __dirname + "/../generated/public_jwks.json",
@@ -24,8 +22,8 @@ jwcrypto.generateKeypair({
       keys: [
         {
           kty: "RSA",
-          n: pubkey.n,
-          e: pubkey.e,
+          n: keypair.rsa.n.toBase64(),
+          e: new BigInteger(keypair.rsa.e.toString(), 10).toBase64(),
           alg: "RS256",
           kid: kid
         }
